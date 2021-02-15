12ce703a7067c148d5efe8a94d438f054bb7a17b
samuele-riva <s.riva72@campus.unimib.it>
2021-02-15 15:02:23 +0100

    fix error

--- src/main/java/com/samueleriva/userLogin/appuser/AppUserRepository.java
+++ src/main/java/com/samueleriva/userLogin/appuser/AppUserRepository.java
@@ -1,6 +1,7 @@
 package com.samueleriva.userLogin.appuser;
 
 import lombok.Data;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,7 +10,7 @@ import java.util.Optional;
 
 @Repository
 @Transactional(readOnly = true)
-public interface AppUserRepository {
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
 
     Optional<AppUser> findByEmail(String email);
 }
