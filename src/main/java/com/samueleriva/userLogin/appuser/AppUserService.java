64c35c7b605f445351bb26bda1a4065d80e92bd1
samuele-riva <s.riva72@campus.unimib.it>
2021-02-15 17:22:31 +0100

    Registration Service

--- src/main/java/com/samueleriva/userLogin/appuser/AppUserService.java
+++ src/main/java/com/samueleriva/userLogin/appuser/AppUserService.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Service;
 public class AppUserService implements UserDetailsService {
 
     private final AppUserRepository appUserRepository;
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     private final static String USER_NOT_FOUND_MSG =
             "user with email %s not found";
@@ -21,5 +24,25 @@ public class AppUserService implements UserDetailsService {
             return appUserRepository.findByEmail(email)
                     .orElseThrow(()->
                             new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG, email)));
+    }
+
+    public String signUpUser(AppUser appUser) {
+        boolean userExist = appUserRepository
+                .findByEmail(appUser.getEmail())
+                .isPresent();
+
+        if(userExist) {
+            throw new IllegalStateException("email already taken");
+        }
+
+        String encodedPassword = bCryptPasswordEncoder.encode((appUser.getPassword()));
+
+        appUser.setPassword(encodedPassword);
+
+        appUserRepository.save(appUser);
+
+        //TODO: Send confirmation token
+
+        return "it works";
     }
 }
--- src/main/java/com/samueleriva/userLogin/registration/RegistrationService.java
+++ src/main/java/com/samueleriva/userLogin/registration/RegistrationService.java
@@ -1,10 +1,33 @@
 package com.samueleriva.userLogin.registration;
 
+import com.samueleriva.userLogin.appuser.AppUser;
+import com.samueleriva.userLogin.appuser.AppUserRole;
+import com.samueleriva.userLogin.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+
+    private final AppUserService appUserService;
+    private EmailValidator emailValidator;
+
     public String register(RegistrationRequest request) {
-        return "works";
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        if(!isValidEmail) {
+            throw new IllegalStateException("email is not valid");
+        }
+
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+
+                )
+        );
     }
 }
